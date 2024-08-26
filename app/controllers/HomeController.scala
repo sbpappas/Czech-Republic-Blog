f7699dd207e5ae33e118715835cc6327b14d1b19
Samuel Pappas <spappas@trinity.edu>
2024-08-26 16:34:10 -0700

    got the basics running

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -21,4 +21,7 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
+  def blog() = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.landing())
+  }
 }
