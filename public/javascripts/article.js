45817d1011f7f301f67ffa3b080bdd5442ac8abb
Samuel Pappas <spappas@trinity.edu>
2024-09-10 20:40:50 -0700

    got another page up but still looks bad

--- public/javascripts/article.js
+++ public/javascripts/article.js
@@ -1,1 +1,36 @@
 console.log("article")
+
+/*const articleData = {
+    title: "Article 1",
+    paragraphs: [
+        "YYYEEEEEE detailed description here.",
+        "More text content related to the article goes here..."
+    ],
+    images: [
+        { src: "/assets/images/photo1.jpg", alt: "photo 1 description" },
+        { src: "/assets/images/photo2.jpg", alt: "photo 2 description" },
+        { src: "/assets/images/photo3.jpg", alt: "photo 3 description" }
+    ]
+};
+
+function ArticlePage() {
+    return React.createElement(
+        'div', { className: 'article-page' },
+        React.createElement('div', { className: 'article-header' }, 
+            React.createElement('h1', null, articleData.title)
+        ),
+        React.createElement('div', { className: 'article-content' },
+            articleData.paragraphs.map((text, index) => 
+                React.createElement('p', { key: index }, text)
+            ),
+            React.createElement('div', { className: 'photo-gallery' },
+                articleData.images.map((img, index) => 
+                    React.createElement('img', { key: index, src: img.src, alt: img.alt })
+                )
+            )
+        )
+    );
+}
+
+ReactDOM.render(React.createElement(ArticlePage, null), document.getElementById('root'));
+*/
